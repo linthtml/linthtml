af17fd2a666875f9277d382daf084b4354723bad
benjaminJ <kamikillerto@gmail.com>
2019-09-03 17:56:24 +0200

    test: Check that the same errors code are reported
    Can not test thee linter output due to an order issue ><

--- test/none_regression/test.js
+++ test/none_regression/test.js
@@ -10,9 +10,12 @@ const html = fs.readFileSync(path.resolve(__dirname, "index.html")).toString("ut
 describe("None regression", function() {
 
   it("LintHTML should report the same errors with the new config and the old config", async function() {
-    const legacy_linter = await linthtml(html, legacy_config);
     const new_linter = await linthtml(html, new_config);
+    const legacy_linter = await linthtml(html, legacy_config);
     expect(legacy_linter.length).to.equal(new_linter.length);
-    expect(legacy_linter).to.deep.equal(new_linter);
+    // expect(legacy_linter).to.deep.equal(new_linter); // Different order ><
+    const a = new_linter.map(_ => parseInt(_.code.replace('E', ''), 10)).sort();
+    const b = legacy_linter.map(_ => parseInt(_.code.replace('E', ''), 10)).sort();
+    expect(a).to.deep.equal(b);
   });
 });
