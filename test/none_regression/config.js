b717f47d11b5d57d0185a986ef1f38d60cb0a4c0
benjaminJ <kamikillerto@gmail.com>
2019-09-04 19:24:44 +0200

    test: Activate more rules for the regression test
    Also add more error in the tested file

--- test/none_regression/config.js
+++ test/none_regression/config.js
@@ -3,113 +3,124 @@ module.exports = {
   "raw-ignore-regex": false,
   "spec-char-escape": false,
   "text-ignore-regex": false,
+  "attr-name-ignore-regex": /^[0-9a-o]+$/,
+  "id-class-ignore-regex": false,
+  "line-max-len-ignore-regex": false,
   "rules": {
     "attr-bans": [
-      true, {
-        "attributes": [
-          "align",
-          "background",
-          "bgcolor",
-          "border",
-          "frameborder",
-          "longdesc",
-          "marginwidth",
-          "marginheight",
-          "scrolling",
-          "style",
-          "width"
-        ]
-      }
+      true,
+      [
+        "align",
+        "background",
+        "bgcolor",
+        "border",
+        "frameborder",
+        "longdesc",
+        "marginwidth",
+        "marginheight",
+        "scrolling",
+        "style",
+        "width"
+      ]
     ],
-    "indent-delta": false,
+    "attr-name-style": [
+      true,
+      "dash"
+    ],
+    "attr-new-line": [
+      true,
+      1
+    ],
+    "attr-no-dup": true,
+    "attr-no-unsafe-char": true,
+    "attr-order": [
+      true,
+      [
+        "id",
+        "class"
+      ]
+    ],
+    "attr-quote-style": [
+      true,
+      "double"
+    ],
+    "attr-req-value": true,
+    "attr-validate": true,
+
+    "class-no-dup": true,
+    "class-style": [
+      true,
+      "lowercase"
+    ],
+
+    "doctype-html5": true,
+    "doctype-first": true,
+
+    "head-valid-content-model": true,
+    "head-req-title": true,
+    "html-req-lang": true,
+    "html-valid-content-model": true,
+    "href-style": [
+      true,
+      "relative"
+    ],
+ 
+    
+    "id-class-no-ad": true,
+    "id-class-style": [
+      true,
+      "underscore"
+    ],
+    "id-no-dup": true,
+    "id-style": [
+      true,
+      "underscore"
+    ],
+    "img-req-alt": true,
+    "img-req-src": true,
+    "indent-delta": true,
     "indent-style": [
       true,
-      {
-        "style": "nonmixed"
-      }
+      "tabs"
     ],
     "indent-width": [
-      true, {
-        "width": 4
-      }
+      true,
+      4
     ],
-    "indent-width-cont": false,
+    "indent-width-cont": true,
+
+    "label-req-for": true,
+    "lang-style": [
+      true,
+      "case"
+    ],
+    "line-end-style": [
+      true,
+      "lf"
+    ],
+    "line-no-trailing-whitespace": true,
+    "line-max-len": [
+      true,
+      80
+    ],
+    "link-req-noopener": true,
+
     "tag-bans": [
       true,
-      {
-        "tags": [
-          "style",
-          "b",
-          "i"
-        ]
-      }
+      [
+        "style",
+        "b",
+        "i"
+      ]
     ],
     "tag-close": true,
     "tag-name-lowercase": true,
     "tag-name-match": true,
-    "tag-self-close": false,
-    "doctype-first": false,
-    "doctype-html5": false,
-    "attr-name-style": [
-      true,
-      {
-        "style": "dash"
-      }
-    ],
-    "attr-name-ignore-regex": false,
-    "attr-no-dup": true,
-    "attr-no-unsafe-char": true,
-    "attr-order": false,
-    "attr-quote-style": [
-      true,
-      {
-        "style": "double"
-      }
-    ],
-    "attr-req-value": true,
-    "attr-new-line": false,
-    "attr-validate": true,
-    "id-no-dup": true,
-    "id-class-no-ad": true,
-    "id-class-style": [
-      true,
-      {
-        "style": "underscore"
-      }
-    ],
-    "class-no-dup": true,
-    "class-style": false,
-    "id-class-ignore-regex": false,
-    "img-req-alt": true,
-    "img-req-src": true,
-    "html-valid-content-model": true,
-    "head-valid-content-model": true,
-    "href-style": false,
-    "link-req-noopener": true,
-    "label-req-for": true,
-    "line-end-style": [
-      true,
-      {
-        "style": "lf"
-      }
-    ],
-    "line-no-trailing-whitespace": true,
-    "line-max-len": false,
-    "line-max-len-ignore-regex": false,
-    "head-req-title": true,
+    "tag-self-close": true,
     "title-no-dup": true,
     "title-max-len": [
       true,
-      {
-        length: 60
-      }
-    ],
-    "html-req-lang": false,
-    "lang-style": [
-      true,
-      {
-        "style": "case"
-      }
+      60
     ],
     "fig-req-figcaption": false,
     "focusable-tabindex-style": false,
