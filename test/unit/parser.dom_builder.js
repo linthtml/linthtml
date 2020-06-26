587c07149454fb7742ebc08bea72e7a9222ef32a
BenjaminJ <kamikillerto@gmail.com>
2020-06-26 15:57:48 +0200

    build: update htmlparser2

--- test/unit/parser.dom_builder.js
+++ test/unit/parser.dom_builder.js
@@ -12,14 +12,6 @@ describe("parser.dom_builder", function() {
     expect(DomBuilder.prototype).to.be.an.instanceOf(DomHandler);
   });
 
-  it("should throw on addDomElement if not initialized with a parser", function() {
-    const builder = new DomBuilder();
-
-    /* eslint-disable no-underscore-dangle */
-    expect(builder._addDomElement.bind(builder)).to.throw(Error);
-    /* eslint-enable no-underscore-dangle */
-  });
-
   describe("onerror", function() {
     it("should throw the error", function() {
       const builder = new DomBuilder();
