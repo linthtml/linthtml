fd0fd9dc28845feb1882734cd0e3a85da21313ae
BenjaminJ <kamikillerto@gmail.com>
2020-07-24 15:08:36 +0200

    chore: s

---
+++ test/none_regression/.linthtmlrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"maxerr": false,
+	"spec-char-escape": false,
+	"text-ignore-regex": false,
+	"raw-ignore-regex": false,
+	"attr-name-ignore-regex": false,
+	"id-class-ignore-regex": false,
+	"line-max-len-ignore-regex": false,
+	"rules": {
+    "indent-width": [
+      true,
+      2
+    ],
+    "indent-style": [
+      true,
+      "tabs"
+    ]
+  }
+}
