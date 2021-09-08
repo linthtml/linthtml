830490a809951b733444d76df3b0960a079abf47
BenjaminJ <kamikillerto@gmail.com>
2021-09-08 16:10:35 +0200

    build: use only conventional config with commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
-    "@commitlint/config-conventional",
-    "@commitlint/config-angular"
+    "@commitlint/config-conventional"
   ],
   rules: {
     "subject-case": [
