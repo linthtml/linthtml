1d34f82ac75132e4363c8b08b9141204ec8709b0
benjaminJ <kamikillerto@gmail.com>
2021-09-18 13:08:32 +0200

    build: Update htmlparse2 to v7

--- packages/dom-utils/dist/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-    is_labelable: require("./is_labelable"),
-    is_void_node: require("./is_void_node"),
-    check_lang_attribute: require("./check_lang_attribute"),
-    ...require("./attr_parse"),
-    ...require("./boolean_attrs"),
-    ...require("./tags")
-};
