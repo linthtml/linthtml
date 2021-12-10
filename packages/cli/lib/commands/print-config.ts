74da924da30d7df9a12c4e28491555a69c846e2f
benjaminJ <kamikillerto@gmail.com>
2021-12-10 16:11:40 +0100

    refactor(core): Setup typescript in `@linthtml/linthtml` package

--- packages/cli/lib/commands/print-config.ts
+++ packages/cli/lib/commands/print-config.ts
@@ -1,7 +1,7 @@
 import chalk from "chalk";
 import { exitProcess, EXIT_CODE_ERROR } from "../utils";
 // @ts-ignore
-import { find_local_config } from "@linthtml/linthtml/lib/read-config";
+import { find_local_config } from "@linthtml/linthtml/dist/lib/read-config";
 
 export default function print_config(path: string) {
   if (path === "") {
