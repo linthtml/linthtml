dff3493080dd78993f5dffd43e6e007a0afeb559
benjaminJ <kamikillerto@gmail.com>
2022-05-05 14:45:07 +0200

    build: fix doc generation build

--- doc/scripts/generate-doc.js
+++ doc/scripts/generate-doc.js
@@ -3,21 +3,20 @@ const fs = require("fs");
 const path = require("path");
 
 const outputDir = "./docs";
-const rulesOutputDir = `${outputDir}/rules`;
+const rulesOutputDir = `${outputDir}/user-guide/rules/rules`;
 
 function main() {
   if (!fs.existsSync(rulesOutputDir)) {
     fs.mkdirSync(rulesOutputDir);
   }
   glob.sync("../README.md").forEach((file) => {
-    const outputFile = path.join(outputDir, file.replace("README.md", "index.md"));
-    fs.copyFileSync(file, outputFile);
+    fs.copyFileSync(file, "./docs/index.md");
   });
 
   glob.sync("../packages/linthtml/lib/rules/**/*.md").forEach((file) => {
     const outputFile = path.join(
       rulesOutputDir,
-      file.replace(/\.\.\/([a-z-]+)\/README.md/, "$1.md").replace("packages/linthtml/lib/", "")
+      file.replace(/([a-z-\d]+)\/README.md/, "$1.md").replace("packages/linthtml/lib/", "")
     );
 
     fs.mkdirSync(path.dirname(outputFile), { recursive: true });
