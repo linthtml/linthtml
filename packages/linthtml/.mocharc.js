febc49f17d44dd11db7295e2ee92a64c7cbc5290
BenjaminJ <kamikillerto@gmail.com>
2023-01-21 12:30:17 +0100

    fix: Correct mocha spec config for linthtml package

--- packages/linthtml/.mocharc.js
+++ packages/linthtml/.mocharc.js
@@ -3,5 +3,5 @@ const baseConfig = require("../../mocharc.base");
 module.exports = {
   ...baseConfig,
   // extends: "./../../mocharc.base.js", // does not work
-  spec: ["__tests__/**/*.test.ts", "lib/rules/**/__tests__/*.ts"]
+  spec: ["lib/__tests__/**/*.test.ts", "lib/rules/**/__tests__/*.ts"]
 };
