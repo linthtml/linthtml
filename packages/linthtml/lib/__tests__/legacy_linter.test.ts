041c6c88ac63752af55fa57d3de48d118eee84e7
BenjaminJ <kamikillerto@gmail.com>
2023-08-11 17:22:55 +0200

    fix: Correct tests still using mocha and chaijs

--- packages/linthtml/lib/__tests__/legacy_linter.test.ts
+++ packages/linthtml/lib/__tests__/legacy_linter.test.ts
@@ -1,4 +1,3 @@
-import { expect } from "chai";
 import Linter from "../legacy/linter";
 import ConstRule from "./fixtures/const_rule";
 import FreeOptionsRule from "../rules/free-options";
@@ -26,7 +25,7 @@ describe("LegacyLinter", function () {
   }
 
   it("should be a function", function () {
-    expect(Linter).to.be.an.instanceOf(Function);
+    expect(Linter).toBeInstanceOf(Function);
   });
 
   describe("lint", function () {
@@ -48,34 +47,34 @@ describe("LegacyLinter", function () {
     ]);
 
     it("Should throw an error when given a nonexistent option", function () {
-      expect(() => createLinter({ nonopt: 7 }).lint("f\nfff")).to.throw('Rule "nonopt" does not exist');
+      expect(() => createLinter({ nonopt: 7 }).lint("f\nfff")).toThrow('Rule "nonopt" does not exist');
     });
 
     it("should return correct line and column numbers", async function () {
       const linter = createLinter();
       linter.rules.addRule(rule);
       const issues = await linter.lint("f\nfff");
-      expect(issues[0].position.start.line).to.be.eql(2);
-      expect(issues[0].position.start.column).to.be.eql(3);
+      expect(issues[0].position.start.line).toEqual(2);
+      expect(issues[0].position.start.column).toEqual(3);
     });
 
     it("should not truncate output if maxerr is not provided", async function () {
       const linter = createLinter();
       linter.rules.addRule(rule);
       const issues = await linter.lint("f\nfff");
-      expect(issues).to.have.length(2);
+      expect(issues).toHaveLength(2);
     });
 
     it("should not return more than the limit fixed by maxerr", async function () {
       const linter = createLinter({ maxerr: 1 });
       linter.rules.addRule(rule);
       const issues = await linter.lint("f\nfff");
-      expect(issues).to.have.length(1);
+      expect(issues).toHaveLength(1);
     });
 
     it("Should throw an error for non-integer config for maxerr", function () {
       // @ts-expect-error Test that config validation throw error is maxerr is not valid
-      expect(() => createLinter({ maxerr: "five" }).lint("")).to.throw(
+      expect(() => createLinter({ maxerr: "five" }).lint("")).toThrow(
         'Configuration for rule "maxerr" is invalid: Expected number got string'
       );
     });
@@ -94,7 +93,7 @@ describe("LegacyLinter", function () {
 
       const output = linter.resetRules();
 
-      expect(output[0]).to.be.eql(issue);
+      expect(output[0]).toEqual(issue);
     });
   });
 });
