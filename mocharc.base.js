f58d5087525923f84032cc81dc389a8985f0e897
BenjaminJ <kamikillerto@gmail.com>
2023-08-11 17:19:46 +0200

    chore: remove mocha and chai

--- mocharc.base.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Specify "require" for CommonJS
-  require: ["ts-node/register", "tsconfig-paths/register"],
-  extensions: ["ts"],
-  spec: ["lib/__tests__/**/*.test.ts"],
-  bail: true
-};
