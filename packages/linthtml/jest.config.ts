2d9a311199e39d9684789af1c648cd13bfb7549c
BenjaminJ <kamikillerto@gmail.com>
2023-08-13 12:57:56 +0200

    fix: coverall action config

--- packages/linthtml/jest.config.ts
+++ packages/linthtml/jest.config.ts
@@ -1,10 +1,12 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
 export default {
   displayName: "linthtml-core",
   preset: "../../jest.preset.js",
 
   testMatch: ["**/lib/__tests__/**/*.test.ts", "**/lib/rules/**/__tests__/*.ts"],
 
-  // preset: '../../../jest.preset.js',
+  coverageDirectory: "../../coverage/linthtml",
   transform: {
     "^.+\\.[tj]s$": [
       "ts-jest",
@@ -13,4 +15,4 @@ export default {
       }
     ]
   }
-};
+} satisfies JestConfigWithTsJest;
