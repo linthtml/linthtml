ff5358a0393264de4c07e23e7782d60048dc58b6
BenjaminJ <kamikillerto@gmail.com>
2024-01-28 12:33:01 +0100

    chore: migrate packages to nx

---
+++ packages/linthtml/src/__tests__/fixtures/custom-parser.js
@@ -0,0 +1,4 @@
+/* 
+ This file is not used directly in tests but it's mandatory to order to mock import with jest
+ */
+module.exports = function() {}
---
+++ packages/linthtml/src/rules/title-no-dup/index.test.ts
@@ -0,0 +1,62 @@
+import { expect } from "chai";
+import linthtml from "../../index.js";
+import { presets } from "../../presets/index.js";
+import { LegacyLinterConfig, RuleConfig } from "../../read-config.js";
+
+describe("legacy linter | title-no-dup", function () {
+  function createLinter(config: LegacyLinterConfig) {
+    return new linthtml.LegacyLinter(linthtml.rules, presets.none, config);
+  }
+  it("Should not report an error when title is not duplicated", async function () {
+    const linter = createLinter({ "title-no-dup": true });
+    const html = "<head><title>Title!</title></head>";
+
+    const issues = await linter.lint(html);
+    expect(issues).to.have.lengthOf(0);
+  });
+
+  it("Should report an error when title is duplicated", async function () {
+    const linter = createLinter({ "title-no-dup": true });
+    const html = "<head><title>Title!</title><title>Title!</title></head>";
+
+    const issues = await linter.lint(html);
+    expect(issues).to.have.lengthOf(1);
+  });
+
+  it("Should catch multiple duplicates", async function () {
+    const linter = createLinter({ "title-no-dup": true });
+    const html = "<head><title>Title!</title><title>Title!</title><title>Title!</title><title>Title!</title></head>";
+
+    const issues = await linter.lint(html);
+    expect(issues).to.have.lengthOf(3);
+  });
+});
+
+describe("title-no-dup", function () {
+  function createLinter(rules: { [rule_name: string]: RuleConfig }) {
+    return linthtml.fromConfig({ rules });
+  }
+  it("Should not report an error when title is not duplicated", async function () {
+    const linter = createLinter({ "title-no-dup": true });
+    const html = "<head><title>Title!</title></head>";
+
+    const issues = await linter.lint(html);
+    expect(issues).to.have.lengthOf(0);
+  });
+
+  it("Should report an error when title is duplicated", async function () {
+    const linter = createLinter({ "title-no-dup": true });
+    const html = "<head><title>Title!</title><title>Title!</title></head>";
+
+    const issues = await linter.lint(html);
+    expect(issues).to.have.lengthOf(1);
+  });
+
+  it("Should catch multiple duplicates", async function () {
+    const linter = createLinter({ "title-no-dup": true });
+    const html = "<head><title>Title!</title><title>Title!</title><title>Title!</title><title>Title!</title></head>";
+
+    const issues = await linter.lint(html);
+    expect(issues).to.have.lengthOf(3);
+  });
+});
