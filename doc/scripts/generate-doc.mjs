c59b1b65dbfcbc01190eecb6284488e3ae10f493
BenjaminJ <kamikillerto@gmail.com>
2024-03-03 11:12:12 +0100

    chore: Fix doc generation

--- doc/scripts/generate-doc.mjs
+++ doc/scripts/generate-doc.mjs
@@ -34,7 +34,7 @@ function main() {
     fs.writeFileSync("./docs/index.md", content, "utf8");
   });
 
-  glob.sync("../packages/linthtml/src/rules/**/*.md").forEach((file) => {
+  glob.sync("../packages/core/src/rules/**/*.md").forEach((file) => {
     const content = remark()
       .use(rewriteLink, {
         rewriter: (url) => url.replace(/(\.\.\/)+doc\/docs/, "").replace(/\.\.\/([a-z-]+)\/README.md/, "$1.md")
@@ -43,7 +43,7 @@ function main() {
       .toString();
     const outputFile = path.join(
       rulesOutputDir,
-      file.replace(/([a-z-\d]+)\/README.md/, "$1.md").replace("../packages/linthtml/src/rules", "")
+      file.replace(/([a-z-\d]+)\/README.md/, "$1.md").replace("../packages/core/src/rules", "")
     );
     fs.mkdirSync(path.dirname(outputFile), { recursive: true });
     fs.writeFileSync(outputFile, content, "utf8");
