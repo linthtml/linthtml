48cf54038a2caa9692a7ff60af60f5288f27747f
BenjaminJ <kamikillerto@gmail.com>
2024-03-05 15:35:26 +0100

    feat: update cosmiconfig to v8
    cosmiconfig v8 support config files in ts, cjs and esm

--- packages/linthtml/src/commands/print-config.ts
+++ packages/linthtml/src/commands/print-config.ts
@@ -2,14 +2,14 @@ import { chalkTemplateStderr } from "chalk-template";
 import { exitProcess, EXIT_CODE_ERROR } from "../utils.js";
 import { find_local_config } from "@linthtml/core";
 
-export default function print_config(path: string) {
+export default async function print_config(path: string) {
   if (path === "") {
     process.stderr.write(
       chalkTemplateStderr`A file path must be provided when using the {blue.bold print-config} option.`
     );
     exitProcess(EXIT_CODE_ERROR);
   }
-  const config = find_local_config(path);
+  const config = await find_local_config(path);
   if (config) {
     process.stdout.write(JSON.stringify(config.config, null, "  "));
     exitProcess();
