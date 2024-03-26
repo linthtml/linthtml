b890ab0d349f66c94b5acf51de817701b50f4f0f
BenjaminJ <kamikillerto@gmail.com>
2024-03-26 15:09:34 +0100

    chore: Reactive custom parser test

--- packages/core/src/__tests__/fixtures/custom-parser.cjs
+++ packages/core/src/__tests__/fixtures/custom-parser.cjs
@@ -1,4 +1,6 @@
 /* 
  This file is not used directly in tests but it's mandatory to order to mock import with jest
  */
-module.exports = function() {}
+module.exports = function (html) {
+  throw new Error(`Custom parser used for "${html}"`);
+};
