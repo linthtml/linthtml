4fe5131ac061856faf8c172762c953ae94b3badb
BenjaminJ <kamikillerto@gmail.com>
2024-06-07 16:30:38 +0200

    chore: Deprecate some rules

--- packages/core/src/rules/indent-delta/index.ts
+++ packages/core/src/rules/indent-delta/index.ts
@@ -1,7 +1,11 @@
+import type { RuleDefinition } from "../../read-config.js";
+
 const RULE_NAME = "indent-delta";
 
 export default {
   name: RULE_NAME,
+  deprecated: true,
+  deprecation_hint: "This rule does nothing and should be removed from the config file",
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   lint() {}
-};
+} satisfies RuleDefinition;
--- packages/core/src/rules/indent-width-cont/index.ts
+++ packages/core/src/rules/indent-width-cont/index.ts
@@ -1,7 +1,10 @@
+import type { RuleDefinition } from "../../read-config.js";
+
 const RULE_NAME = "indent-width-cont";
 
 export default {
   name: RULE_NAME,
+  deprecated: true,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   lint() {}
-};
+} satisfies RuleDefinition;
