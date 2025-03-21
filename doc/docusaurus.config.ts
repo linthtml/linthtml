d899986d1a98515dd91a36b2f92588ba0339c3f4
BenjaminJ <kamikillerto@gmail.com>
2025-03-21 10:16:28 +0100

    docs: Change prismjs theme

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -125,7 +125,7 @@ const config: Config = {
     //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
     // },
     prism: {
-      theme: prismThemes.github,
+      theme: prismThemes.dracula,
       darkTheme: prismThemes.dracula
     }
   } satisfies Preset.ThemeConfig,
