338251d87767267eb6761c9c944fbdf8b2bb8f32
BenjaminJ <kamikillerto@gmail.com>
2025-03-21 11:31:34 +0100

    docs: Add deprecation notice for legacy rule settings

--- packages/core/src/rules/text-ignore-regex/index.ts
+++ packages/core/src/rules/text-ignore-regex/index.ts
@@ -1,12 +1,16 @@
+import type { RuleDefinition } from "../../read-config.js";
+
 import { create_string_or_regexp_validator } from "../../validate_option.js";
 
 const RULE_NAME = "text-ignore-regex";
 
 export default {
   name: RULE_NAME,
+  deprecated: true,
+  deprecation_hint: "This is not a rule, it's a deprecated setting doing nothing currently.",
   validateConfig: create_string_or_regexp_validator(RULE_NAME),
   lint() {}
-};
+} as RuleDefinition;
 
 // var regex = /[^\\](&[^a-zA-Z0-9#;]*;)|([&<>])|(&[a-zA-Z0-9#]*[^a-zA-Z0-9#;])/gm;
 // const regex = /\\[^a-zA-Z0-9\-_ \n\r\s\\]/g;
