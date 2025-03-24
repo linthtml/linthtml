280be7d7b93f5d0dc97301af2ab9e3cf24de0903
BenjaminJ <kamikillerto@gmail.com>
2025-03-24 10:38:53 +0100

    docs: Update "attr-name-ignore-regex" deprecation message

--- packages/core/src/rules/attr-name-ignore-regex/index.ts
+++ packages/core/src/rules/attr-name-ignore-regex/index.ts
@@ -6,7 +6,7 @@ const RULE_NAME = "attr-name-ignore-regex";
 export default {
   deprecated: true,
   deprecation_hint:
-    "This is not a rule. It's a setting that will be used by real rules to ignore some attribute based on their names",
+    'This is not a rule, but a setting used by the rule "attr-name-style". It can can be replaced by defining "ignore" in "attr-name-style" rule config',
   name: RULE_NAME,
   validateConfig: create_string_or_regexp_validator(RULE_NAME),
   lint() {}
