be91a26468a0b5acd5c2496a513be4be5d45b83a
BenjaminJ <kamikillerto@gmail.com>
2025-03-26 14:43:50 +0100

    feat: Update id-style config to allow providing object config with format and ignore pattern

--- packages/core/src/rules/id-style/index.test.ts
+++ packages/core/src/rules/id-style/index.test.ts
@@ -17,6 +17,80 @@ describe("legacy linter | id-style", function () {
   //   expect(issues).to.have.lengthOf(0);
   // });
 
+  it("Should throw an error if `id-class-ignore-regex` is empty", function () {
+    const linter = createLinter({
+      "id-style": "dash",
+      "id-class-ignore-regex": ""
+    });
+    const html = '<div id="bar-2"></div>';
+
+    expect(() => linter.lint(html)).to.throw(
+      'Configuration for rule "id-class-ignore-regex" is invalid: You provide an empty string value'
+    );
+  });
+
+  it("should throw an error if rule config is empty", function () {
+    const linter = createLinter({ "id-style": "" });
+
+    expect(() => linter.lint("")).to.throw(
+      'Configuration for rule "id-style" is invalid: "" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
+    );
+  });
+
+  it("should throw an error if rule config is provided with an invalid format", function () {
+    const linter = createLinter({ "id-style": "foo" });
+
+    expect(() => linter.lint("")).to.throw(
+      'Configuration for rule "id-style" is invalid: "foo" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
+    );
+  });
+
+  it("should throw an error if object config as invalid key", function () {
+    const linter = createLinter({ "id-style": { foo: "" } });
+
+    expect(() => linter.lint("")).to.throw(
+      'Object configuration for rule "id-style" is invalid: key "foo" is not accepted, only "format", "ignore" are.'
+    );
+  });
+
+  it("Should throw an error when the value for the config key format is not valid", function () {
+    const linter = createLinter({ "id-style": { format: ["camel"] } });
+
+    expect(() => linter.lint("")).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "format" is not valid: Expected string or RegExp got array'
+    );
+  });
+
+  it("Should throw an error when the value for the config key ignore is not valid", function () {
+    const linter = createLinter({ "id-style": { format: "camel", ignore: 1 } });
+
+    expect(() => linter.lint("")).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "ignore" is not valid: Expected string or RegExp got number'
+    );
+  });
+
+  it("Should throw an error when the key 'format' is missing in the config object", function () {
+    const linter = createLinter({ "id-style": { ignore: "" } });
+
+    expect(() => linter.lint("")).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "format" is missing'
+    );
+  });
+
+  it("Should throw an error when an invalid format is passed within the object config", function () {
+    const linter = createLinter({ "id-style": { format: "foo" } });
+
+    expect(() => linter.lint("")).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "format" is not valid: "foo" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem".'
+    );
+  });
+
+  it("Should not throw an error when a valid config object is provided", function () {
+    const linter = createLinter({ "id-style": { format: "camel" } });
+
+    expect(() => linter.lint("")).to.not.throw();
+  });
+
   it("Should not report any error for correctly formatted id", async function () {
     const linter = createLinter({ "id-style": "lowercase" });
     const html = '<div id="foo"></div>';
@@ -27,7 +101,7 @@ describe("legacy linter | id-style", function () {
 
   describe("'lowercase' format", function () {
     it("Should not report an error for ids with valid format", async function () {
-      const linter = createLinter({ "id-style": "lowercase" });
+      const linter = createLinter({ "id-style": { format: "lowercase" } });
       const html = '<div id="foo"></div>';
       const issues = await linter.lint(html);
       expect(issues).to.have.lengthOf(0);
@@ -131,18 +205,6 @@ describe("legacy linter | id-style", function () {
     expect(issues[0].code).to.equal("DEPRECATED_RULE");
   });
 
-  it("Should throw an error if `id-class-ignore-regex` is empty", function () {
-    const linter = createLinter({
-      "id-style": "dash",
-      "id-class-ignore-regex": ""
-    });
-    const html = '<div id="bar-2"></div>';
-
-    expect(() => linter.lint(html)).to.throw(
-      'Configuration for rule "id-class-ignore-regex" is invalid: You provide an empty string value'
-    );
-  });
-
   it("Rule should not fail if id attribute has no value", function () {
     const linter = createLinter({ "id-style": "dash" });
     const html = `
@@ -150,22 +212,6 @@ describe("legacy linter | id-style", function () {
     `;
 
     expect(() => linter.lint(html)).to.not.throw();
-  });
-
-  it("should throw an error if rule config is empty", function () {
-    const linter = createLinter({ "id-style": "" });
-
-    expect(() => linter.lint("")).to.throw(
-      'Configuration for rule "id-style" is invalid: "" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
-    );
-  });
-
-  it("should throw an error if rule config is provided with an invalid format", function () {
-    const linter = createLinter({ "id-style": "foo" });
-
-    expect(() => linter.lint("")).to.throw(
-      'Configuration for rule "id-style" is invalid: "foo" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
-    );
   });
 });
 
@@ -189,6 +235,72 @@ describe("id-style", function () {
   //   expect(issues).to.have.lengthOf(0);
   // });
 
+  it("should throw an error if rule config is empty", function () {
+    const config = {
+      "id-style": [true, ""] as [boolean, unknown]
+    };
+
+    expect(() => createLinter(config)).to.throw(
+      'Configuration for rule "id-style" is invalid: "" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
+    );
+  });
+
+  it("should throw an error if rule config is provided with an invalid format", function () {
+    const config = {
+      "id-style": [true, "foo"] as [boolean, unknown]
+    };
+
+    expect(() => createLinter(config)).to.throw(
+      'Configuration for rule "id-style" is invalid: "foo" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
+    );
+  });
+
+  it("should throw an error if object config as invalid key", function () {
+    const config = { "id-style": [true, { foo: "" }] } satisfies Record<string, RuleConfig>;
+
+    expect(() => createLinter(config)).to.throw(
+      'Object configuration for rule "id-style" is invalid: key "foo" is not accepted, only "format", "ignore" are.'
+    );
+  });
+
+  it("Should throw an error when the value for the config key format is not valid", function () {
+    const config = { "id-style": [true, { format: ["camel"] }] } satisfies Record<string, RuleConfig>;
+
+    expect(() => createLinter(config)).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "format" is not valid: Expected string or RegExp got array'
+    );
+  });
+
+  it("Should throw an error when the value for the config key ignore is not valid", function () {
+    const config = { "id-style": [true, { format: "camel", ignore: 1 }] } satisfies Record<string, RuleConfig>;
+
+    expect(() => createLinter(config)).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "ignore" is not valid: Expected string or RegExp got number'
+    );
+  });
+
+  it("Should throw an error when the key 'format' is missing in the config object", function () {
+    const config = { "id-style": [true, { ignore: "" }] } satisfies Record<string, RuleConfig>;
+
+    expect(() => createLinter(config)).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "format" is missing'
+    );
+  });
+
+  it("Should throw an error when an invalid format is passed within the object config", function () {
+    const config = { "id-style": [true, { format: "foo" }] } satisfies Record<string, RuleConfig>;
+
+    expect(() => createLinter(config)).to.throw(
+      'Object configuration for rule "id-style" is invalid: Setting "format" is not valid: "foo" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem".'
+    );
+  });
+
+  it("Should not throw an error when a valid config object is provided", function () {
+    const config = { "id-style": [true, { format: "camel" }] } satisfies Record<string, RuleConfig>;
+
+    expect(() => createLinter(config)).to.not.throw();
+  });
+
   it("Should not report any error for correctly formatted id", async function () {
     const linter = createLinter({
       "id-style": [true, "lowercase"]
@@ -202,7 +314,7 @@ describe("id-style", function () {
   describe("'lowercase' format", function () {
     it("Should not report an error for ids with valid format", async function () {
       const linter = createLinter({
-        "id-style": [true, "lowercase"]
+        "id-style": [true, { format: "lowercase" }]
       });
       const html = '<div id="foo"></div>';
       const issues = await linter.lint(html);
@@ -334,25 +446,5 @@ describe("id-style", function () {
     expect(issues).to.have.lengthOf(1);
     expect(issues[0].severity).to.equal("warning");
     expect(issues[0].code).to.equal("DEPRECATED_RULE");
-  });
-
-  it("should throw an error if rule config is empty", function () {
-    const config = {
-      "id-style": [true, ""] as [boolean, unknown]
-    };
-
-    expect(() => createLinter(config)).to.throw(
-      'Configuration for rule "id-style" is invalid: "" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
-    );
-  });
-
-  it("should throw an error if rule config is provided with an invalid format", function () {
-    const config = {
-      "id-style": [true, "foo"] as [boolean, unknown]
-    };
-
-    expect(() => createLinter(config)).to.throw(
-      'Configuration for rule "id-style" is invalid: "foo" is not accepted. Accepted values are "none", "lowercase", "underscore", "dash", "camel" and "bem"'
-    );
   });
 });
--- packages/core/src/rules/id-style/index.ts
+++ packages/core/src/rules/id-style/index.ts
@@ -1,7 +1,11 @@
 import match_format from "../../utils/check_format.js";
 import { is_tag_node } from "@linthtml/dom-utils";
-import type { reportFunction, RuleDefinition } from "../../read-config.js";
-import { create_list_value_validator } from "../../validate_option.js";
+import type { LegacyLinterConfig, reportFunction, RuleDefinition } from "../../read-config.js";
+import {
+  create_list_value_validator,
+  create_object_validator,
+  create_string_or_regexp_validator
+} from "../../validate_option.js";
 import { types } from "node:util";
 import type { Node } from "@linthtml/dom-utils/dom_elements";
 
@@ -9,14 +13,26 @@ const { isRegExp } = types;
 
 const RULE_NAME = "id-style";
 
+type RULE_CONFIG_SIMPLE = "none" | "lowercase" | "underscore" | "dash" | "camel" | "bem" | RegExp;
+type RULE_CONFIG_EXTENDED = { format: RULE_CONFIG_SIMPLE; ignore?: string | RegExp };
+type RULE_CONFIG = RULE_CONFIG_SIMPLE | RULE_CONFIG_EXTENDED;
+
+function is_extended_config(config: RULE_CONFIG): config is RULE_CONFIG_EXTENDED {
+  return typeof config === "object" && !isRegExp(config) && !Array.isArray(config);
+}
+
 // TODO: Check behavoir with empty id attribute
 function lint(
   node: Node,
-  format: string | RegExp,
-  { report, global_config }: { report: reportFunction; global_config: Record<string, unknown> }
+  config: RULE_CONFIG,
+  { report, global_config }: { report: reportFunction; global_config: LegacyLinterConfig }
 ) {
+  const format = is_extended_config(config) ? config.format : config;
+
   if (is_tag_node(node) && format !== "none") {
-    const ignore = global_config["id-class-ignore-regex"] as undefined | string | RegExp;
+    const ignore = is_extended_config(config)
+      ? config.ignore ?? global_config["id-class-ignore-regex"]
+      : global_config["id-class-ignore-regex"];
 
     // TODO: Remove after `raw-ignore-text` refacto
     let attributes = node.attributes
@@ -48,8 +64,50 @@ function lint(
   }
 }
 
+function validateConfig(config: RULE_CONFIG) {
+  if (is_extended_config(config)) {
+    create_object_validator(RULE_NAME, ["format", "ignore"])(config);
+
+    if (!config.format) {
+      throw new Error(`Object configuration for rule "${RULE_NAME}" is invalid: Setting "format" is missing`);
+    }
+
+    try {
+      create_list_value_validator(
+        RULE_NAME,
+        ["none", "lowercase", "underscore", "dash", "camel", "bem"],
+        true
+      )(config.format);
+    } catch (error) {
+      const error_message = (error as Error).message
+        .replace("Configuration for", "Object configuration for")
+        .replace(":", ': Setting "format" is not valid:');
+      throw error_message;
+    }
+
+    if (config.ignore) {
+      try {
+        create_string_or_regexp_validator(RULE_NAME)(config.ignore);
+      } catch (error) {
+        const error_message = (error as Error).message
+          .replace("Configuration for", "Object configuration for")
+          .replace(":", ': Setting "ignore" is not valid:');
+        throw error_message;
+      }
+    }
+
+    return config;
+  } else {
+    return create_list_value_validator(
+      RULE_NAME,
+      ["none", "lowercase", "underscore", "dash", "camel", "bem"],
+      true
+    )(config);
+  }
+}
+
 export default {
   name: RULE_NAME,
-  validateConfig: create_list_value_validator(RULE_NAME, ["none", "lowercase", "underscore", "dash", "camel", "bem"]),
+  validateConfig,
   lint
 } as RuleDefinition;
