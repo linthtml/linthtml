39125673cd8b8cb613272f0d8adae5eece9bc115
BenjaminJ <kamikillerto@gmail.com>
2026-04-30 15:42:13 +0200

    ci: Update commitlint config to enfore scope matching package names

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,17 @@
-module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ],
-  rules: {
-    "subject-case": [
-      0,
-      "always",
-      "sentence-case"
-    ]
-  }
-};
+async function getConfig() {
+  const {
+    default: {
+      utils: { getProjects }
+    }
+  } = await import("@commitlint/config-nx-scopes");
+
+  console.log();
+  return {
+    rules: {
+      "subject-case": [0, "always", "sentence-case"],
+      "scope-enum": async (ctx) => [2, "always", ["deps", "release", "ci", "doc", ...(await getProjects(ctx))]]
+    }
+  };
+}
+
+module.exports = getConfig();
